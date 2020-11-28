87ca00291a4cc6ceb6ce170b327b4ff41921cfcd
AaronXue <her8153101@yahoo.com.tw>
2020-11-29 07:12:10 +0800

    Update login buttons
    Update facebook login button
    
    Update google login button

--- src/components/Profile/GoogleSignInBtn.vue
+++ src/components/Profile/GoogleSignInBtn.vue
@@ -1,12 +1,20 @@
 <template>
-  <GoogleLogin
-    :params="params"
-    :renderParams="renderParams"
-    :onSuccess="onSuccess"
-    :onFailure="onFailure"
-  ></GoogleLogin>
-
-  <!-- <GoogleLogin :params="params" :logoutButton="true">Logout</GoogleLogin> -->
+  <div>
+    <GoogleLogin
+      v-if="!isLogined"
+      :params="params"
+      :renderParams="renderParams"
+      :onSuccess="onSuccess"
+      :onFailure="onFailure"
+      :logoutButton="isLogined"
+      :onCurrentUser="getCurernt"
+    ></GoogleLogin>
+    <v-btn v-else text @click="logout">
+      <GoogleLogin  :params="params" :logoutButton="true">
+        Logout
+      </GoogleLogin>
+    </v-btn>
+  </div>
 </template>
 
 <script>
@@ -17,21 +25,25 @@ export default {
     },
     data() {
     return {
+        isLogined: false,
         params: {
             client_id: "784954252883-pa2ldt93uuctvt84f53nrepau4ullh0d.apps.googleusercontent.com"
         },
         // only needed if you want to render the button with the google ui
         renderParams: {
-            width: 150,
-            height: 50,
-            longtitle: false
+            width: 236.16,
+            height: 42,
+            longtitle: true
         }
     };
   },
   methods:{
-    onSuccess(googleUser) {
+    getCurernt(googleUser){
         console.log(googleUser);
-
+        if(!googleUser) this.isLogined = false
+    },
+    onSuccess(googleUser) {
+        this.isLogined = true
         // This only gets the user information: id, name, imageUrl and email
         console.log(googleUser.getBasicProfile());
     },
@@ -40,6 +52,10 @@ export default {
 
         // This only gets the user information: id, name, imageUrl and email
         console.log(googleUser.getBasicProfile());
+    },
+    logout(){
+        console.log(this.isLogined)
+        this.isLogined = false
     }
   }
 };
