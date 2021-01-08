3293b357094bc2129cd8c27529b6b31a3cee9845
AaronXue0 <her8153101@yahoo.com.tw>
2021-01-08 23:20:16 +0800

    Refactor profile template

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,24 +1,11 @@
 <template>
   <v-container>
-    <v-row justify="center">
-      <v-card-title>
-        Game Store
-      </v-card-title>
-    </v-row>
     <v-row
       v-if="checktLogin != null"
       justify="center"
       style="position:relative;top:-25px"
     >
-      <v-col cols="2"></v-col>
-      <v-col cols="6">
-        <v-toolbar elevation="0">
-          <v-spacer />
-          <v-btn @click="logout" elevation="0" color="white" text>
-            <span class="blue--text">Log out</span>
-          </v-btn>
-          <v-spacer />
-        </v-toolbar>
+      <v-col cols="9">
         <v-row align="center">
           <v-item-group
             v-model="window"
@@ -37,89 +24,115 @@
 
           <v-col>
             <v-window v-model="window" class="elevation-1" vertical>
-              <v-window-item v-for="n in length" :key="n">
+              <v-window-item :value="0">
                 <v-card flat>
                   <v-card-text>
                     <v-row class="mb-4" align="center">
-                      <v-avatar color="grey" class="mr-4"></v-avatar>
-                      <strong class="title">Title {{ n }}</strong>
-                      <v-spacer></v-spacer>
-                      <v-btn icon>
+                      <v-toolbar elevation="0">
+                        <v-card-title>
+                          Game Store
+                        </v-card-title>
                         <v-icon>mdi-account</v-icon>
-                      </v-btn>
+                        <v-spacer />
+                        <v-btn @click="logout" elevation="0" color="white" text>
+                          <span class="blue--text">Log out</span>
+                        </v-btn>
+                      </v-toolbar>
                     </v-row>
 
-                    <p>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                      sed do eiusmod tempor incididunt ut labore et dolore magna
-                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                      ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                      Duis aute irure dolor in reprehenderit in voluptate velit
-                      esse cillum dolore eu fugiat nulla pariatur. Excepteur
-                      sint occaecat cupidatat non proident, sunt in culpa qui
-                      officia deserunt mollit anim id est laborum.
-                    </p>
-
-                    <p>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                      sed do eiusmod tempor incididunt ut labore et dolore magna
-                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                      ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                      Duis aute irure dolor in reprehenderit in voluptate velit
-                      esse cillum dolore eu fugiat nulla pariatur. Excepteur
-                      sint occaecat cupidatat non proident, sunt in culpa qui
-                      officia deserunt mollit anim id est laborum.
-                    </p>
-
-                    <p>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                      sed do eiusmod tempor incididunt ut labore et dolore magna
-                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                      ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                      Duis aute irure dolor in reprehenderit in voluptate velit
-                      esse cillum dolore eu fugiat nulla pariatur. Excepteur
-                      sint occaecat cupidatat non proident, sunt in culpa qui
-                      officia deserunt mollit anim id est laborum.
-                    </p>
+                    <h2 class="text-center">
+                      <u>個人檔案</u>
+                    </h2>
+                    <br />
+                    <v-form>
+                      <v-text-field
+                        outlined
+                        label="信箱"
+                        disabled
+                        :value="checktLogin.email"
+                      >
+                      </v-text-field>
+                      <v-text-field
+                        outlined
+                        label="姓名"
+                        disabled
+                        :value="checktLogin.displayName"
+                      ></v-text-field>
+                      <v-text-field outlined label="地址一"></v-text-field>
+                      <v-text-field outlined label="地址二"></v-text-field>
+                      <v-text-field outlined label="國家"></v-text-field>
+                      <v-text-field outlined label="城市"></v-text-field>
+                      <v-text-field outlined label="Zip"></v-text-field>
+                      <v-text-field outlined label="聯絡電話"></v-text-field>
+                      <v-toolbar elevation="0">
+                        <v-spacer />
+                        <v-btn text>
+                          Save
+                        </v-btn>
+                      </v-toolbar>
+                    </v-form>
                   </v-card-text>
                 </v-card>
               </v-window-item>
+              <v-window-item :value="1">
+                <v-row class="mb-4" align="center">
+                  <v-toolbar elevation="0">
+                    <v-card-title>
+                      Game Store
+                    </v-card-title>
+                    <v-icon>mdi-account</v-icon>
+                    <v-spacer />
+                    <v-btn @click="logout" elevation="0" color="white" text>
+                      <span class="blue--text">Log out</span>
+                    </v-btn>
+                  </v-toolbar>
+                </v-row>
+                <v-row>
+                  <v-col cols="1"> </v-col>
+                  <v-col>
+                    <v-card-subtitle>
+                      現有優惠券
+                    </v-card-subtitle>
+                  </v-col>
+                </v-row>
+                <v-row v-for="(item, index) in coupons" :key="index">
+                  <v-col cols="1"></v-col>
+                  <v-col cols="1">
+                    <v-divider
+                      class="mx-4"
+                      vertical
+                      style="display: inline"
+                    ></v-divider>
+                  </v-col>
+                  <v-col cols="7">
+                    <h4>{{ item }}</h4>
+                  </v-col>
+                  <v-spacer />
+                  <v-col cols="2" style="left: -10px">
+                    <h4>x1</h4>
+                  </v-col>
+                </v-row>
+                <br />
+                <br />
+              </v-window-item>
+              <!-- <v-window-item :value="2">
+                <v-row class="mb-4" align="center">
+                  <v-toolbar elevation="0">
+                    <v-card-title>
+                      Game Store
+                    </v-card-title>
+                    <v-icon>mdi-account</v-icon>
+                    <v-spacer />
+                    <v-btn @click="logout" elevation="0" color="white" text>
+                      <span class="blue--text">Log out</span>
+                    </v-btn>
+                  </v-toolbar>
+                </v-row>
+              </v-window-item> -->
             </v-window>
           </v-col>
         </v-row>
-        <h2 class="text-center">
-          <u>個人檔案</u>
-        </h2>
-        <br />
-        <v-form>
-          <v-text-field
-            outlined
-            label="信箱"
-            disabled
-            :value="checktLogin.email"
-          >
-          </v-text-field>
-          <v-text-field
-            outlined
-            label="姓名"
-            disabled
-            :value="checktLogin.displayName"
-          ></v-text-field>
-          <v-text-field outlined label="地址一"></v-text-field>
-          <v-text-field outlined label="地址二"></v-text-field>
-          <v-text-field outlined label="國家"></v-text-field>
-          <v-text-field outlined label="城市"></v-text-field>
-          <v-text-field outlined label="Zip"></v-text-field>
-          <v-text-field outlined label="聯絡電話"></v-text-field>
-          <v-toolbar elevation="0">
-            <v-spacer />
-            <v-btn text>
-              Save
-            </v-btn>
-          </v-toolbar>
-        </v-form>
       </v-col>
-      <v-col cols="2"></v-col>
     </v-row>
     <v-row v-else justify="center">
       <firebaseSingn />
@@ -137,8 +150,9 @@ export default {
   name: "Home",
   data() {
     return {
-      length: 3,
+      length: 2,
       window: 0,
+      coupons: ["滿千折百", "滿一送醫"],
     };
   },
   components: {
